77207eb0ffdef6a386711be6585aee790986932a
N'yoma Diamond <34525496+nyoma-diamond@users.noreply.github.com>
2022-02-21 16:38:20 -0500

    data generation

---
+++ data/gen.py
@@ -0,0 +1,25 @@
+import sys
+import random
+
+vals = 10000
+if(len(sys.argv) > 1):
+    vals = sys.argv[1]
+else:
+    print(f"Specify arg for custom data size. Using {vals} as default.")
+
+vals = int(vals)
+
+people = open("output/PEOPLE.csv", "w")
+i_small = open("output/INFECTED-small.csv", "w")
+i_large = open("output/INFECTED-large.csv", "w")
+for i in range(vals):
+    write_str = f"{i}," + ",".join([str(random.randint(1, 10000)) for j in range(2)]) + "\n"
+    people.write(write_str)
+    if i % 1000 == 0:
+        i_large.write(write_str)
+        if i < vals/4:
+            i_small.write(write_str)
+
+people.close()
+i_small.close()
+i_large.close()
