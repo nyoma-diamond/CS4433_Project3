78d644628c3e16b504a8c3d01ecc67c9980f7a5f
N'yoma Diamond <34525496+nyoma-diamond@users.noreply.github.com>
2022-02-22 17:24:52 -0500

    removed commented out code

--- src/main/scala/P1Q1.scala
+++ src/main/scala/P1Q1.scala
@@ -1,28 +1,3 @@
-
-//import org.apache.spark.SparkConf
-//import org.apache.spark.sql.SparkSession
-
-//object RunMain extends  Serializable{
-//  def main(args: Array[String]): Unit = {
-//    val spark = init_sc()
-//    val sc = spark.sparkContext
-//    P1Q1.run(sc)
-//    sc.stop()
-//  }
-//
-//  def init_sc(): SparkSession ={
-//    val conf = new SparkConf().setAppName("cs585")
-//    conf.set("spark.sql.parquet.compression.codec", "uncompressed")
-//    conf.set("spark.serializer", "org.apache.spark.serializer.KryoSerializer")
-//    val spark = SparkSession
-//      .builder()
-//      .config(conf)
-//      .getOrCreate()
-//    spark
-//  }
-//}
-
-
 import org.apache.spark.SparkContext
 import org.apache.spark.sql.SparkSession
 
